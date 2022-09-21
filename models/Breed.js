4cfea05f3b0ce84f905445bac63d6bb392fb73c5
Dakota Jones <dakota32jones@gmail.com>
2022-09-20 21:25:03 -0500

    added Breed and Names Models

---
+++ models/Breed.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequilize');
+const sequilize = require('../config/connection');
+
+class Breed extends Model {}
+
+Breed.init(
+    {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        temperament: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+        },
+
+        life_span: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequilize, 
+        timestamps: false,
+        freezeTablename: true, 
+        underscored: true,
+        modelName: 'breed'
+    }
+);
+
+module.exports = Breed;
