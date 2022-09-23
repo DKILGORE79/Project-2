c32b5c9f47f3e583f5d64f9dddb23471139b72b7
Dakota Jones <dakota32jones@gmail.com>
2022-09-22 19:06:49 -0500

    fixed lint errors

--- models/Breed.js
+++ models/Breed.js
@@ -4,36 +4,36 @@ const sequilize = require('../config/connection');
 class Breed extends Model {}
 
 Breed.init(
-    {
-        id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
-        temperament: {
-            type: DataTypes.STRING, 
-            allowNull: false,
-        },
-
-        life_span: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequilize, 
-        timestamps: false,
-        freezeTablename: true, 
-        underscored: true,
-        modelName: 'breed'
-    }
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    temperament: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequilize,
+    timestamps: false,
+    freezeTablename: true,
+    underscored: true,
+    modelName: 'breed',
+  }
 );
 
 module.exports = Breed;
--- models/Names.js
+++ models/Names.js
@@ -4,26 +4,26 @@ const sequilize = require('../config/connection');
 class Names extends Model {}
 
 Names.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequilize, 
-        timestamps: false,
-        freezeTablename: true, 
-        underscored: true,
-        modelName: 'breed'
-    }
+
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequilize,
+    timestamps: false,
+    freezeTablename: true,
+    underscored: true,
+    modelName: 'breed',
+  }
 );
 
 module.exports = Names;
