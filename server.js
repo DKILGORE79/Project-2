f1588924849709087573d8815512d5681b2939a4
Derek Kilgore <derek.kilgore@gmail.com>
2022-09-27 19:36:42 -0500

    merging

--- server.js
+++ server.js
@@ -61,15 +61,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'handlebars');
 app.set(express.static('public'));
 
-// app.use('/random-name', (req, res) => {
-//   const { first_name } = data[Math.round(Math.random() * data.length)];
-//   return res.json({ first_name });
-// });
-
-// app.use('/', (req, res) => {
-//   return res.render('index');
-// });
-
 app.listen(3001, () => console.log('App listening...'));
 
 
