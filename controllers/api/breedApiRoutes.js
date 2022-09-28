65d4bbd7a586e12e9966bb978b29d599b9999318
Derek Kilgore <98043070+DKILGORE79@users.noreply.github.com>
2022-09-27 19:53:43 -0500

    Update breedApiRoutes.js

--- controllers/api/breedApiRoutes.js
+++ controllers/api/breedApiRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const axios = require('axios').default;
 const { getDogBreeds } = require('../../utils/dogApi');
 router.get('/', async (_req, res) => {
   try {
