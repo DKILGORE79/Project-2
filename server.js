06ea7e1bcf4d0fcbdfee14899d85d1920e82cb40
Derek Kilgore <98043070+DKILGORE79@users.noreply.github.com>
2022-09-29 18:15:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(cors());
+
 // Inform Express.js on which template engine to use
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
